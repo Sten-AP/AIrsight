f5648cdcec5a8baf04fc05540ada1ca8c4773e1f
bnchy <s123836@ap.be>
2023-11-08 15:26:38 +0000

    fix: added test and training accuracy scores

--- model/model_building/linear_regression.py
+++ model/model_building/linear_regression.py
@@ -2,13 +2,15 @@ import pandas as pd
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 import matplotlib.pyplot as plt
 import seaborn as sns 
 
 training_data = pd.read_csv("model\calls\datasets\wekeo_data.csv")
 target_data = pd.read_csv("model\calls\datasets\openAQ_data.csv")
 
-training_data.drop("time", axis=1, inplace=True)
+#training_data.drop("time", axis=1, inplace=True)
+
 
 print("=======training data=======")
 print(training_data.head())
@@ -28,13 +30,24 @@ print(X)
 print("=======Y=======")
 print(Y)
 
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.3, random_state=42)
+
+
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.3, random_state=41)
 model = LinearRegression()
 model.fit(X_train, Y_train)
 predictions = model.predict(X_test) 
 
+print("=======predictions=======")
+print(predictions)
+print("=======Y_test=======")
+print(Y_test)
+
+training_accuracy = model.score(X_train,Y_train)
+print("training_accuracy is: ",training_accuracy)
+print("test_accuracy is: ",model.score(X_test,Y_test))
 print('mean_squared_error : ', mean_squared_error(Y_test, predictions)) 
 print('mean_absolute_error : ', mean_absolute_error(Y_test, predictions)) 
+print('r2_score : ', r2_score(Y_test, predictions))
 
 plt.figure(figsize=(8, 6))
 sns.regplot(x=Y_test, y=predictions, scatter_kws={'color':'blue'}, line_kws={'color':'orange'})
