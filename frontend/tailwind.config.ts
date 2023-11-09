8d9c7ae3f0f1e59553476dbb6f7e54b199128afc
Viral Ganga Persad <viral.ganga@gmail.com>
2023-11-09 11:49:33 +0100

    Fix MODULE NOT FOUND server.js in frontend

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -2,9 +2,9 @@ import type { Config } from 'tailwindcss'
 
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
     "./node_modules/@tremor/**/*.{js,ts,jsx,tsx}", // Tremor module
   ],
   darkMode: "class",
