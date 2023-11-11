528bed0018777707cec26808b4a16b7ec60ff70b
St3N1 <94611203+Sten-AP@users.noreply.github.com>
2023-11-11 15:37:37 +0100

    Dockerfile wekeo done & changes to main

--- backend/WEkEO/main.py
+++ backend/WEkEO/main.py
@@ -10,12 +10,12 @@ import os
 import numpy as np
 from query import query_settings
 import shutil
+from dotenv import load_dotenv
 
 BASE_DIR = os.path.dirname(__file__)
 DATA_DIR = BASE_DIR+"./data"
 NOW = pd.Timestamp.now(tz='UCT').strftime('%Y-%m-%d')
 
-URL_INFLUDB = "http://localhost:8086"
 ORG = "AP"
 TOKEN = "I1SYSyvrMEmwP88EPLjxxxeQrk14TdIqid8Y4XmeEmqTveJczU5r9nb4x9l37JtXP0xt64-h5oZrplJN01jPMw=="
 BUCKET = "WEkEO"
@@ -47,10 +47,10 @@ def main():
     load_dotenv()
     config = Configuration(user=os.getenv("USERNAME_WEKEO"), password=os.getenv("PASSWORD"))
     hda_client = Client(config=config)
-    write_client = InfluxDBClient3(host=URL_INFLUDB, token=TOKEN,
+    write_client = InfluxDBClient3(host=os.getenv("URL_INFLUXDB"), token=TOKEN,
                             org=ORG, database=BUCKET, enable_gzip=True)
     
-    read_client = InfluxDBClient(url=URL_INFLUDB, token=TOKEN, org=ORG)
+    read_client = InfluxDBClient(url=os.getenv("URL_INFLUXDB"), token=TOKEN, org=ORG)
     read_api = read_client.query_api()
     
     influxdb_query = """
