261f39efd293f13a41fd742faec0a1acd29904b2
St3N1 <94611203+Sten-AP@users.noreply.github.com>
2023-11-11 15:10:16 +0100

    Fix cpu usage with time.sleep

--- backend/OpenAQ/main.py
+++ backend/OpenAQ/main.py
@@ -2,7 +2,6 @@ from pandas import Timestamp, DataFrame
 from dotenv import load_dotenv
 import requests
 import os
-import schedule
 import time
 
 load_dotenv()
@@ -10,29 +9,28 @@ load_dotenv()
 response = requests.get(url=os.getenv("OPENAQ_URL"), headers={"accept": "application/json"})
 
 def main():
-    sensoren = []
-    for result in response.json()['results']:
-        sensor = {
-            'id': result['id'],
-            'name': result['name'],
-            'lat': result['coordinates']['latitude'],
-            'lon': result['coordinates']['longitude'],
-            'time': str(Timestamp.now(tz='UCT').floor('ms'))
-        }
-        
-        for parameter in result["parameters"]:
-            if parameter["lastValue"] > -1:
-                sensor.update({parameter["parameter"]: parameter["lastValue"]})
-        
-        sensoren.append(sensor)
-        
-    sensoren_json = DataFrame(sensoren).to_json(orient="split")
-    print(requests.post(os.getenv("API_URL") + f"/sensor/new/", json={"data": sensoren_json}).json())
+    while True:
+        sensoren = []
+        for result in response.json()['results']:
+            sensor = {
+                'id': result['id'],
+                'name': result['name'],
+                'lat': result['coordinates']['latitude'],
+                'lon': result['coordinates']['longitude'],
+                'time': str(Timestamp.now(tz='UCT').floor('ms'))
+            }
+            
+            for parameter in result["parameters"]:
+                if parameter["lastValue"] > -1:
+                    sensor.update({parameter["parameter"]: parameter["lastValue"]})
+            
+            sensoren.append(sensor)
+            
+        sensoren_json = DataFrame(sensoren).to_json(orient="split")
+        print(requests.post(os.getenv("API_URL") + f"/sensor/new/", json={"data": sensoren_json}).json())
+        time.sleep(1800)
 
 
 if __name__ == "__main__":
-    schedule.every().hour.do(main)
-    while True:
-        schedule.run_pending()
-        time.sleep(600)
+    main()
 
