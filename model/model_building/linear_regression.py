ff7b1a787a12f259b115dc224e1156ec9bd1d42a
Talzion42 <79468094+Talzion42@users.noreply.github.com>
2023-11-15 16:12:16 +0100

    fixed dataset pathing for pycharm usage

--- model/model_building/linear_regression.py
+++ model/model_building/linear_regression.py
@@ -4,10 +4,10 @@ from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 import matplotlib.pyplot as plt
-import seaborn as sns 
+import seaborn as sns
 
-training_data = pd.read_csv("model\calls\datasets\wekeo_data.csv")
-target_data = pd.read_csv("model\calls\datasets\openAQ_data.csv")
+training_data = pd.read_csv('../calls/datasets/wekeo_data.csv')
+target_data = pd.read_csv('../calls/datasets/openAQ_data.csv')
 
 #training_data.drop("time", axis=1, inplace=True)
 
@@ -18,7 +18,7 @@ print(training_data.columns)
 
 print("=======target data=======")
 print(target_data.head())
-print(target_data.columns)  
+print(target_data.columns)
 
 target_variable = "pm10"
 
@@ -35,7 +35,7 @@ print(Y)
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.3, random_state=41)
 model = LinearRegression()
 model.fit(X_train, Y_train)
-predictions = model.predict(X_test) 
+predictions = model.predict(X_test)
 
 print("=======predictions=======")
 print(predictions)
@@ -45,8 +45,8 @@ print(Y_test)
 training_accuracy = model.score(X_train,Y_train)
 print("training_accuracy is: ",training_accuracy)
 print("test_accuracy is: ",model.score(X_test,Y_test))
-print('mean_squared_error : ', mean_squared_error(Y_test, predictions)) 
-print('mean_absolute_error : ', mean_absolute_error(Y_test, predictions)) 
+print('mean_squared_error : ', mean_squared_error(Y_test, predictions))
+print('mean_absolute_error : ', mean_absolute_error(Y_test, predictions))
 print('r2_score : ', r2_score(Y_test, predictions))
 
 plt.figure(figsize=(8, 6))
