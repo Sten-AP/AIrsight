c2d15447326828d54e51d6f074726e3dcc8e2d8a
Talzion42 <joppe.vanbelleghem@hotmail.com>
2023-11-19 16:00:24 +0100

    changed model saving method

--- model/model_building/linear_regression.py
+++ model/model_building/linear_regression.py
@@ -5,11 +5,13 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 import matplotlib.pyplot as plt
 import seaborn as sns
+import joblib
+from sklearn2pmml import PMMLPipeline, sklearn2pmml
 
 training_data = pd.read_csv('../calls/datasets/wekeo_data.csv')
 target_data = pd.read_csv('../calls/datasets/openAQ_data.csv')
 
-#training_data.drop("time", axis=1, inplace=True)
+# training_data.drop("time", axis=1, inplace=True)
 
 
 print("=======training data=======")
@@ -22,15 +24,13 @@ print(target_data.columns)
 
 target_variable = "pm10"
 
-X =  training_data
+X = training_data
 Y = target_data[target_variable]
 
 print("=======X=======")
 print(X)
 print("=======Y=======")
 print(Y)
-
-
 
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.3, random_state=41)
 model = LinearRegression()
@@ -42,23 +42,26 @@ print(predictions)
 print("=======Y_test=======")
 print(Y_test)
 
-training_accuracy = model.score(X_train,Y_train)
-print("training_accuracy is: ",training_accuracy)
-print("test_accuracy is: ",model.score(X_test,Y_test))
+training_accuracy = model.score(X_train, Y_train)
+print("training_accuracy is: ", training_accuracy)
+print("test_accuracy is: ", model.score(X_test, Y_test))
 print('mean_squared_error : ', mean_squared_error(Y_test, predictions))
 print('mean_absolute_error : ', mean_absolute_error(Y_test, predictions))
 print('r2_score : ', r2_score(Y_test, predictions))
 
 plt.figure(figsize=(8, 6))
-sns.regplot(x=Y_test, y=predictions, scatter_kws={'color':'blue'}, line_kws={'color':'orange'})
+sns.regplot(x=Y_test, y=predictions, scatter_kws={'color': 'blue'}, line_kws={'color': 'orange'})
 plt.xlabel(f'actual {target_variable} values')
 plt.ylabel(f'predicted {target_variable} Values')
 plt.title('regression Line')
 plt.show()
 
-from sklearn2pmml import PMMLPipeline, sklearn2pmml
 # package iris classifier model with PMML
-sklearn2pmml(PMMLPipeline([("estimator",
-                        	model)]),
-         	"iris_model.pmml",
-         	with_repr=True)
+# sklearn2pmml(PMMLPipeline([("estimator",
+#                             model)]),
+#              "linear_regression.pmml",
+#              with_repr=True)
+
+# save the model
+filename = 'linear_model_2.sav'
+joblib.dump(model, open(filename, 'wb'))
