7790912fafd097cc97df142a0bd4d112142f3501
Sten Hulsbergen <s122441@ap.be>
2023-11-21 11:22:06 +0100

    Change code to more functions

--- backend/WEkEO/main.py
+++ backend/WEkEO/main.py
@@ -19,7 +19,8 @@ API_URL = getenv("WEKEO_API_URL")
 
 BASE_DIR = path.dirname(__file__)
 DATA_DIR = f"{BASE_DIR}/data"
-NOW = Timestamp.now(tz='UCT').strftime('%Y-%m-%d')
+DAYS = 1
+MAX_THREADS = 10
 
 
 if path.exists(DATA_DIR):
@@ -36,70 +37,79 @@ def get_sensor_locations():
         locations.append({'id': sensor['id'], 'lon': sensor['lon'], 'lat': sensor['lat']})              
     return locations
 
+def start_and_end_date(days):
+    start_date = (Timestamp.now(tz='UCT') - DateOffset(days+1)).strftime('%Y-%m-%d')
+    end_date = (Timestamp.now(tz='UCT') - DateOffset(days)).strftime('%Y-%m-%d')
+    return [start_date, end_date]
 
-def main(id, lat, lon, days):
-    start_date = (Timestamp(NOW, tz='UCT') - DateOffset(days+1)).strftime('%Y-%m-%d')
-    end_date = (Timestamp(NOW, tz='UCT') - DateOffset(days)).strftime('%Y-%m-%d')
+def download_data(id, lat, lon, days):
+    date = start_and_end_date(days)
     
-    print(f"[>] {start_date} -> {end_date} [<] searching lat: {lat}, lon: {lon}")
-    query = query_settings(lat, lon, start_date, end_date)
+    print(f"[>] {date[0]} -> {date[1]} [<] searching lat: {lat}, lon: {lon}")
+    query = query_settings(lat, lon, date[0], date[1])
     matches = hda_client.search(query)
         
     print("downloading data")
-    matches.download(DATA_DIR)
-    print(matches.results)
-    nc_files = [f for f in listdir(DATA_DIR) if f.endswith('.nc')]
-    data_file = nc.Dataset(f"{DATA_DIR}/{nc_files[0]}")
-    # data_file = matches.results
-        
-    data = {}
-    for key in data_file.variables.keys():
-        if key != "level":
-            d = data_file.variables[key][:]
-            normal_array = np.where(d.mask, None, d)
-            result = [float(x) for x in normal_array.flatten() if x is not None]
-            
-            data.update({key: result})
+    matches.download(f"{DATA_DIR}/{id}")
 
-    wekeo = []
-    for i in range(len(data['time'])):
-        hour = str(int(data['time'][i]) - (24 * (int(data['time'][i]) // 24)))
-        if len(hour) == 1:
-            hour = f"0{hour}"
+def post_data(start_date):
+    id_dirs = listdir(DATA_DIR)
+    for id_dir in id_dirs:
+        nc_files = [f for f in listdir(f"{DATA_DIR}/{id_dir}") if f.endswith('.nc')]
+    
+        for nc_file in nc_files:
+            data_file = nc.Dataset(f"{DATA_DIR}/{id_dir}/{nc_file}")
             
-        date = (Timestamp(start_date) + DateOffset((int(data['time'][i]) // 24))).strftime('%Y-%m-%d')
+            data = {}
+            for key in data_file.variables.keys():
+                if key != "level":
+                    d = data_file.variables[key][:]
+                    normal_array = np.where(d.mask, None, d)
+                    result = [float(x) for x in normal_array.flatten() if x is not None]
+                    
+                    data.update({key: result})
 
-        data_dict = {
-            'id': id,
-            'lon': data['longitude'][0],
-            'lat': data['latitude'][0],
-            'time': str(Timestamp(f"{date}T{hour}:00:00.000Z"))
-        }
-            
-        for key in data_file.variables.keys():
-            if key not in ['longitude', 'latitude', 'time', 'level']:
-                data_dict.update({key: data[key][i]})    
-        wekeo.append(data_dict)
-            
-    print("sending data to database")
-    wekeo_json = DataFrame(wekeo).to_json(orient="split")        
-    print(Session().post(f"{API_URL}/wekeosensor/new/", json={"data": wekeo_json}).json())
-    remove(f"{DATA_DIR}/{nc_files[0]}")
+            wekeo = []
+            for i in range(len(data['time'])):
+                hour = str(int(data['time'][i]) - (24 * (int(data['time'][i]) // 24)))
+                if len(hour) == 1:
+                    hour = f"0{hour}"
+                    
+                date = (Timestamp(start_date) + DateOffset((int(data['time'][i]) // 24))).strftime('%Y-%m-%d')
+
+                data_dict = {
+                    'id': id_dir,
+                    'lon': data['longitude'][0],
+                    'lat': data['latitude'][0],
+                    'time': str(Timestamp(f"{date}T{hour}:00:00.000Z"))
+                }
+                    
+                for key in data_file.variables.keys():
+                    if key not in ['longitude', 'latitude', 'time', 'level']:
+                        data_dict.update({key: data[key][i]})    
+                wekeo.append(data_dict)
+                    
+            print("sending data to database")
+            wekeo_json = DataFrame(wekeo).to_json(orient="split")        
+            print(Session().post(f"{API_URL}/wekeosensor/new/", json={"data": wekeo_json}).json())
+
+def main():
+    sensor_locations = get_sensor_locations()
+    for sensor in sensor_locations:
+        active_threads = threading.active_count()
+        if active_threads <= MAX_THREADS:
+            worker = threading.Thread(target=download_data, args=(sensor['id'], sensor['lat'],sensor['lon'], DAYS,))
+            worker.start()
+        print(f"Threads active: {active_threads}")
+        while active_threads == MAX_THREADS:
+            sleep(10)
+                    
+    post_data(start_and_end_date(DAYS)[0])
+    
 
 
 if __name__ == "__main__":
     config = Configuration(user=USERNAME, password=PASSWORD)
     hda_client = Client(config=config, progress=True)
-    
-    sensor_locations = get_sensor_locations()
-    main(sensor_locations[0]['id'], sensor_locations[0]['lat'],sensor_locations[0]['lon'], 1)
-    # maxthreads = 1
-    # days = 1
-    # for sensor in sensor_locations:
-    #     active_threads = threading.active_count()
-    #     if active_threads <= maxthreads:
-    #         worker = threading.Thread(target=main, args=(sensor['id'], sensor['lat'],sensor['lon'], days,))
-    #         worker.start()
-    #     print(f"Threads active: {active_threads}")
-    #     while active_threads == maxthreads:
-    #         sleep(10)
+    main()
+
