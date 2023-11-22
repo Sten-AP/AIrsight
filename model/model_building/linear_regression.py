ce0c6ea85245e1f404a2b77ca05cc24b8594e2c1
bnchy <s123836@ap.be>
2023-11-22 10:33:54 +0000

    chore: restructured saving and loading of the model

--- model/model_building/linear_regression.py
+++ model/model_building/linear_regression.py
@@ -1,3 +1,4 @@
+import os
 import pandas as pd
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
@@ -6,19 +7,42 @@ import joblib
 
 training_data = pd.read_csv("model\calls\datasets\wekeo_data.csv")
 target_data = pd.read_csv("model\calls\datasets\openAQ_data.csv")
-merged_data = pd.merge(training_data, target_data, on='local_date', how='inner')
+merged_data = pd.merge(training_data, target_data, on="local_date", how="inner")
 merged_data.drop("local_date", axis=1, inplace=True)
 
 best_combinations = {
-    "pm10": ["pm25_x", "pm25_y","pm10_x", "pm10_y", "no2_x", "no2_y", "so2", "co_conc", "nmvoc"],
-    "pm25": ['pm10_x', 'pm10_y', 'pm25_x', 'pm25_y', 'time_x', 'time_y', 'no2_x', 'no2_y', 'so2', 'nmvoc'],
+    "pm10": [
+        "pm25_x",
+        "pm25_y",
+        "pm10_x",
+        "pm10_y",
+        "no2_x",
+        "no2_y",
+        "so2",
+        "co_conc",
+        "nmvoc",
+    ],
+    "pm25": [
+        "pm10_x",
+        "pm10_y",
+        "pm25_x",
+        "pm25_y",
+        "time_x",
+        "time_y",
+        "no2_x",
+        "no2_y",
+        "so2",
+        "nmvoc",
+    ],
     "no2": ["pm25_x", "pm25_y", "no2_x", "no2_y", "so2", "nmvoc", "co_conc"],
     "o3": ["time"],
     "so2": ["no2_x", "no2_y", "co_conc", "nmvoc", "no"],
-    "co": ["pm25_x","pm25_y", "no2_x", "no2_y", "so2", "nmvoc", "no"],
+    "co": ["pm25_x", "pm25_y", "no2_x", "no2_y", "so2", "nmvoc", "no"],
     "nmvoc": ["pm25_x", "pm25_y", "no2_x", "no2_y", "so2", "co_conc", "no"],
-    "no": ["pm25_x", "pm25_y","no2_x", "no2_y", "so2", "co_conc", "nmvoc"]
+    "no": ["pm25_x", "pm25_y", "no2_x", "no2_y", "so2", "co_conc", "nmvoc"],
 }
+
+filename = ""
 
 print("=======training data=======")
 print(training_data.head())
@@ -30,16 +54,16 @@ print(target_data.columns)
 
 print("=======merged data=======")
 print(merged_data.head())
-print(merged_data.columns)  
+print(merged_data.columns)
 
 target_variable = input("enter the target variable (pm25, pm10, no2): ")
 
-if (target_variable == "pm25_y"):
-    merged_data= merged_data[best_combinations["pm25"]]
-elif (target_variable == "pm10_y"):
-    merged_data= merged_data[best_combinations["pm10"]]
-elif (target_variable == "no2_y"):
-    merged_data= merged_data[best_combinations["no2"]]
+if target_variable == "pm25_y":
+    merged_data = merged_data[best_combinations["pm25"]]
+elif target_variable == "pm10_y":
+    merged_data = merged_data[best_combinations["pm10"]]
+elif target_variable == "no2_y":
+    merged_data = merged_data[best_combinations["no2"]]
 
 X = merged_data.drop(target_variable, axis=1)
 Y = merged_data[target_variable]
@@ -50,21 +74,36 @@ print("=======Y=======")
 print(Y)
 
 
-
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.3, random_state=42)
+X_train, X_test, Y_train, Y_test = train_test_split(
+    X, Y, test_size=0.3, random_state=42
+)
 model = LinearRegression()
 model.fit(X_train, Y_train)
-predictions = model.predict(X_test)
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+saved_models_dir = os.path.join(current_dir, "saved_models")
+if not os.path.exists(saved_models_dir):
+    os.makedirs(saved_models_dir)
+
+if target_variable == "pm25_y":
+    filename = "linear_model_pm25.sav"
+elif target_variable == "pm10_y":
+    filename = "linear_model_pm10.sav"
+elif target_variable == "no2_y":
+    filename = "linear_model_no2.sav"
+
+joblib.dump(model, os.path.join(saved_models_dir, filename))
+
+loaded_model = joblib.load(os.path.join(saved_models_dir, filename))
+predictions = loaded_model.predict(X_test)
+
 
 print("=======predictions=======")
 print(predictions)
 print("=======Y_test=======")
 print(Y_test)
-training_accuracy = model.score(X_train,Y_train)
-print("training_accuracy is: ",training_accuracy)
-print("test_accuracy is: ",model.score(X_test,Y_test))
-print('mean_squared_error : ', mean_squared_error(Y_test, predictions)) 
-print('mean_absolute_error : ', mean_absolute_error(Y_test, predictions)) 
-
-filename = 'linear_model_2.sav'
-joblib.dump(model, open(filename, 'wb'))
+training_accuracy = model.score(X_train, Y_train)
+print("training_accuracy is: ", training_accuracy)
+print("test_accuracy is: ", model.score(X_test, Y_test))
+print("mean_squared_error : ", mean_squared_error(Y_test, predictions))
+print("mean_absolute_error : ", mean_absolute_error(Y_test, predictions))
