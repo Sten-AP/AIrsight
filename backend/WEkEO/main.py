afb0269689fef7fe3a5fe23f2e0897b3d3af938d
Talzion42 <79468094+Talzion42@users.noreply.github.com>
2023-11-22 16:54:42 +0100

    added initial documentation for wekeo, updated .gitignore with .idea, fixed some minor IDE problems in wekeo main.py

--- backend/WEkEO/main.py
+++ backend/WEkEO/main.py
@@ -1,4 +1,4 @@
-import netCDF4 as nc
+import netCDF4 as nC
 from dotenv import load_dotenv
 from hda import Client, Configuration
 from pandas import Timestamp, DateOffset, DataFrame
@@ -19,14 +19,14 @@ BASE_DIR = path.dirname(__file__)
 DATA_DIR = f"{BASE_DIR}/data"
 NOW = Timestamp.now(tz='UCT').strftime('%Y-%m-%d')
 
-
 if path.exists(DATA_DIR):
     rmtree(DATA_DIR)
+
 
 def get_sensor_locations(response):
     locations = []
     for sensor in response:
-        locations.append({'id': sensor['id'], 'lon': sensor['lon'], 'lat': sensor['lat']})              
+        locations.append({'id': sensor['id'], 'lon': sensor['lon'], 'lat': sensor['lat']})
     return locations
 
 
@@ -34,27 +34,27 @@ def main():
     config = Configuration(user=USERNAME, password=PASSWORD)
     hda_client = Client(config=config)
     session = Session()
-    
+
     try:
         response = get(f"{API_URL}/openaqsensor/").json()
     except Exception as e:
         print(f"Data not found: {e}")
-        
+
     sensor_locations = get_sensor_locations(response)
 
     start_date = (Timestamp(NOW, tz='UCT') - DateOffset(1)).strftime('%Y-%m-%d')
     end_date = Timestamp(NOW, tz='UCT').strftime('%Y-%m-%d')
-    
+
     for i, sensor in enumerate(sensor_locations):
         print(f"searching lat: {sensor['lat']}, lon: {sensor['lon']}")
         query = query_settings(sensor['lat'], sensor['lon'], start_date, end_date)
         matches = hda_client.search(query)
-        
+
         print("downloading data")
         matches.download(DATA_DIR)
-    
+
         nc_files = [f for f in listdir(DATA_DIR) if f.endswith('.nc')]
-        data_file = nc.Dataset(f"{DATA_DIR}/{nc_files[i]}")
+        data_file = nC.Dataset(f"{DATA_DIR}/{nc_files[i]}")
 
         data = {}
         for key in data_file.variables.keys():
@@ -62,7 +62,7 @@ def main():
                 d = data_file.variables[key][:]
                 normal_array = np.where(d.mask, None, d)
                 result = [float(x) for x in normal_array.flatten() if x is not None]
-            
+
                 data.update({key: result})
 
         wekeo = []
@@ -70,7 +70,7 @@ def main():
             hour = str(int(data['time'][i]) - (24 * (int(data['time'][i]) // 24)))
             if len(hour) == 1:
                 hour = f"0{hour}"
-            
+
             date = (Timestamp(start_date) + DateOffset((int(data['time'][i]) // 24))).strftime('%Y-%m-%d')
 
             data_dict = {
@@ -79,14 +79,14 @@ def main():
                 'lat': data['latitude'][0],
                 'time': str(Timestamp(f"{date}T{hour}:00:00.000Z"))
             }
-            
+
             for key in data_file.variables.keys():
                 if key not in ['longitude', 'latitude', 'time', 'level']:
-                    data_dict.update({key: data[key][i]})    
+                    data_dict.update({key: data[key][i]})
             wekeo.append(data_dict)
-            
+
         print("sending data to database")
-        wekeo_json = DataFrame(wekeo).to_json(orient="split")        
+        wekeo_json = DataFrame(wekeo).to_json(orient="split")
         print(session.post(f"{API_URL}/wekeosensor/new/", json={"data": wekeo_json}).json())
 
 
---
+++ documentation/WEkEO.md
@@ -0,0 +1,20 @@
+# WEkEO Documentation
+## What is WEkEO?
+
+WEkEO is one of the European DIAS (Data and Information Access services), which allows a centralised access to all Copernicus data and large computations on a cloud infrastructure.
+
+It is an online cloud platform that collates and presents sattelite data with a focus on environmental aspects. 
+It's possible to request a wide variety of satellite data through their cloud infrastructure using Jupyter Notebooks, but this isn't as easy for every layperson.
+
+This is why we are developing our own cloud platform that utilizes data from, among other sources, WEkEO, to present their data in a slightly more user-friendly manner.
+
+## What does out WEkEO script do?
+
+Running the WEkEO script does the following:
+
+1. Calls our own API to retrieve location data on each of the OpenAQ ground sensors.
+2. Uses this location data to call the WEkEO API and retrieve satellite data surrounding every individual ground sensor.  
+3. Save and write the data from the WEkEO API to the database, using the same id as the sensor of which the location data was used.
+
+This script doesn't run amazingly fast, since we do individual WEkEO calls for each OpenAQ sensor present in the database.  
+It does however, provide us with complete and valuable data to later use in any frontend applications. All in combination with the ground sensor data from OpenAQ.
