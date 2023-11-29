93e5de8276370676ab5f03e0e88dfa0313d840cf
Sten H <s122441@ap.be>
2023-11-29 12:42:20 +0100

    Add geomapping & change tag columns

--- backend/FastAPI/main.py
+++ backend/FastAPI/main.py
@@ -7,7 +7,7 @@ from pandas import read_json
 from uvicorn import run
 from io import StringIO
 
-## TIME FILTER FORMAT FOR REQUEST: 2023-11-15T12:00:00.00
+# TIME FILTER FORMAT FOR REQUEST: 2023-11-15T12:00:00.00
 
 # -----------App-settings-----------
 app = FastAPI(
@@ -38,32 +38,38 @@ app.add_middleware(
 )
 
 # -----------Routes-----------
+
+
 @app.post("/api/{param}/new/", tags=["Add item"])
 async def post_new_data(param: str, data: Data):
     if param not in PARAMETERS:
         return {"error": "parameter does not match"}
-    
+
     data_df = read_json(StringIO(data.data), orient="split").set_index('time')
     try:
         if param == "openaqsensor":
-            write_client.write(data_df, data_frame_measurement_name=param, data_frame_tag_columns=['country', 'id'])
+            write_client.write(data_df, data_frame_measurement_name=param,
+                               data_frame_tag_columns=['country', 'country_code', 'id', 'region'])
         if param == "wekeosensor":
-            write_client.write(data_df, data_frame_measurement_name=param, data_frame_tag_columns=['id'])
+            write_client.write(
+                data_df, data_frame_measurement_name=param, data_frame_tag_columns=['id'])
         return {"message": f"{param} data succesfully added to database"}
     except Exception as e:
         return {"message": f"error with adding {param} data to database: {e}"}
+
 
 @app.get("/api/{param}/", tags=["Latest data"])
 async def get_data_by_param(param: str):
     if param not in PARAMETERS:
         return {"error": "parameter does not match"}
-    
+
     try:
         query = get_query(param)
         response = read_api.query(query, org=ORG)
         return list_all_items(response)
     except Exception as e:
         return {"error": str(e)}
+
 
 @app.get("/api/{param}/{id}/", tags=["Specific data (with timestamps)"])
 async def get_all_data_by_param_and_id(param: str, id: str, dates: Dates = None):
@@ -76,32 +82,35 @@ async def get_all_data_by_param_and_id(param: str, id: str, dates: Dates = None)
     else:
         start_date = None
         stop_date = None
-        
+
     try:
-        query = get_query(param=param, id=id, start_date=start_date, stop_date=stop_date)
+        query = get_query(param=param, id=id,
+                          start_date=start_date, stop_date=stop_date)
         response = read_api.query(query, org=ORG)
         return list_all_items(response, start_date, stop_date)
     except Exception as e:
         return {"error": str(e)}
 
+
 @app.get("/api/{param}/{id}/{data}/", tags=["Specific data (with timestamps)"])
 async def get_specific_data_by_param_and_id(param: str, id: str, data: str, dates: Dates = None):
     if param not in PARAMETERS:
         return {"error": "parameter does not match"}
-    
+
     if dates != None:
         start_date = f"{dates.start_date}Z"
         stop_date = f"{dates.stop_date}Z"
     else:
         start_date = None
         stop_date = None
-    
+
     try:
-        query = get_query(param=param, id=id, data=data, start_date=start_date, stop_date=stop_date)
+        query = get_query(param=param, id=id, data=data,
+                          start_date=start_date, stop_date=stop_date)
         response = read_api.query(query, org=ORG)
         return list_all_items(response, start_date, stop_date)
     except Exception as e:
         return {"error": str(e)}
 
 if __name__ == "__main__":
-    run("main:app", host="0.0.0.0", port=6000, reload=True, proxy_headers=True, forwarded_allow_ips=['*'])
+    run("main:app", host="0.0.0.0", port=6000, reload=True, proxy_headers=True, forwarded_allow_ips=['*'], workers=8)
--- backend/OpenAQ/main.py
+++ backend/OpenAQ/main.py
@@ -3,11 +3,23 @@ from dotenv import load_dotenv
 from requests import Session, get
 from os import getenv
 from time import sleep
+from geopy.geocoders import Nominatim
 
 load_dotenv()
 
 OPENAQ_URL = getenv("OPENAQ_URL")
 API_URL = getenv("OPENAQ_API_URL")
+
+
+def get_address_by_location(latitude, longitude, language="en"):
+    """This function returns an address as raw from a location
+    will repeat until success"""
+    coordinates = f"{latitude}, {longitude}"
+    try:
+        return geo.reverse(coordinates, language=language).raw
+    except:
+        return get_address_by_location(latitude, longitude)
+
 
 def main():
     session = Session()
@@ -16,9 +28,19 @@ def main():
         sensoren = []
         timestamp = Timestamp.now(tz='UCT').floor('ms')
         for result in response.json()['results']:
+            address = get_address_by_location(
+                result['coordinates']['latitude'], result['coordinates']['longitude'])['address']
+            
+            if address.get("state") is not None:
+                state = address["state"]
+            else:
+                state = address["region"]
+                
             sensor = {
                 'id': result['id'],
-                'country': result["country"],
+                'region': state,
+                'country': address["country"],
+                'country_code': address["country_code"].upper(),
                 'lat': result['coordinates']['latitude'],
                 'lon': result['coordinates']['longitude'],
                 'time': str(Timestamp(f"{timestamp.date()}T{timestamp.hour}:00:00.000Z"))
@@ -27,17 +49,19 @@ def main():
             for parameter in result["parameters"]:
                 if parameter["lastValue"] > -1:
                     sensor.update({parameter["parameter"]: parameter["lastValue"]})
-            
+
             sensoren.append(sensor)
             
         sensoren_json = DataFrame(sensoren).to_json(orient="split")
+        print(sensoren_json)
         try:
-            print(session.post(f"{API_URL}/api/openaqsensor/new/", json={"data": sensoren_json}).json())
+            print(session.post(f"{API_URL}/openaqsensor/new/", json={"data": sensoren_json}).json())
         except Exception as e:
             print(f"Error posting data: {e}")
         sleep(1800)
 
 
 if __name__ == "__main__":
+    geo = Nominatim(user_agent="airsight")
     main()
 
