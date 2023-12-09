f3b4626215fac3b265b8668fa1bcb3c3c2af1fc8
St3N1 <94611203+Sten-AP@users.noreply.github.com>
2023-12-09 14:09:19 +0100

    Add ORJSON for faster api responses

--- backend/FastAPI/setup.py
+++ backend/FastAPI/setup.py
@@ -4,7 +4,9 @@ from os import getenv, path
 from influxdb_client_3 import InfluxDBClient3
 from influxdb_client import InfluxDBClient
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.middleware.gzip import GZipMiddleware
 from fastapi import FastAPI
+
 
 
 load_dotenv()
@@ -45,10 +47,16 @@ app = FastAPI(
         },
     ]
 )
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
+)
+
+app.add_middleware(
+    GZipMiddleware,
+    minimum_size=1000,
 )
