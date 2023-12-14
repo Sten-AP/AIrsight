bd49d21c23e54e0df4364524c13783d863c1c1b3
JelleSpruyt <jelle.spruyt@student.ap.be>
2023-12-14 19:00:23 +0100

    feat: expanded cli and documentation for model building, allowed the saving of both wekeo and openaq's csv's by running one main.py file

--- new_model/models/code/openAQ.py
+++ new_model/models/code/openAQ.py
@@ -4,13 +4,19 @@ import pandas as pd
 import os
 from datetime import datetime, timedelta
 
-URL_OPENAQ = "https://api.openaq.org/v2/measurements?format=json&date_from=2023-10-02T01%3A00%3A00-16%3A00&date_to=2023-10-4T08%3A00%3A00-16%3A00&page=1&offset=0&limit=100000&sort=desc&radius=1000&country=BE&location_id={}&order_by=datetime"
-HEADERS = {"accept": "application/json"}
-DATASET_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "..", "datasets", "multiple_sensors")
-NOW = pd.Timestamp.now(tz='UCT').strftime('%Y-%m-%d')
 
-def fetch_sensor_data(sensor_id):
-    response = requests.get(url=URL_OPENAQ.format(sensor_id), headers=HEADERS)
+
+def fetch_sensor_data(sensor_id, start_date, end_date):
+    #setup
+    URL_OPENAQ = "https://api.openaq.org/v2/measurements?format=json&date_from={}&date_to={}&page=1&offset=0&limit=100000&sort=desc&radius=1000&country=BE&location_id={}&order_by=datetime"
+    HEADERS = {"accept": "application/json"}
+    DATASET_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "..", "datasets", "multiple_sensors")
+    NOW = pd.Timestamp.now(tz='UCT').strftime('%Y-%m-%d')
+
+    #api-call
+    response = requests.get(url=URL_OPENAQ.format(start_date, end_date, sensor_id), headers=HEADERS)
+    print(response.json())
+    latitude, longitude = 0, 0
     sensors_dict = {}  
     sensors_list = []
     for data in response.json()['results']:
@@ -37,6 +43,8 @@ def fetch_sensor_data(sensor_id):
                 'latitude': data['coordinates']['latitude'],
                 'longitude': data['coordinates']['longitude']
             }
+            latitude = data['coordinates']['latitude']
+            longitude = data['coordinates']['longitude']
                 
     for local_date, values in sensors_dict.items():
         local_date_converted = datetime.fromisoformat(local_date)
@@ -52,5 +60,10 @@ def fetch_sensor_data(sensor_id):
             'latitude': values['latitude'],
             'longitude': values['longitude']
         })
-    sensors_df = pd.DataFrame(sensors_list).set_index('time')
-    sensors_df.to_csv(os.path.join(DATASET_DIR, "openAQ_data.csv"), index=False)
+    if sensors_list:
+        sensors_df = pd.DataFrame(sensors_list).set_index('time')
+        sensors_df.to_csv(os.path.join(DATASET_DIR, "openAQ_data.csv"), index=False)
+    else:
+        print("No data received from the API.")
+        latitude, longitude = None, None
+    return latitude, longitude
--- new_model/models/main.py
+++ new_model/models/main.py
@@ -1,4 +1,5 @@
 # this is the main file used for both doing calls and building models based on the data from the calls
+# NOTE: make sure to have a .env file in the root of the project with the following variables: USERNAME_WEKEO: <your username>, PASSWORD: <your password>
 from code.wekeo import wekeo_api_call
 from code.openAQ import fetch_sensor_data
 
@@ -8,21 +9,24 @@ print("Welcome to AIrsight model building!")
 print("Please choose between the following options:")
 print("1. Build a model based on one custom sensor")
 print("2. Build a model based on multiple sensors")
-choice = input("Please enter your choice: ")
+choice = int(input("Please enter your choice: "))
+validChoice = False
 sensors = []
-if (choice ==1): 
-    print("Please enter the sensor id of the sensor you want to build a model for: ")
-    sensor_id = input()
-elif (choice ==2): 
-    print("You've chosen multiple sensors, building a model with 5 sensors in belgium...")
-    sensor_ids = [3036, 4463, 4861, 4926, 4878]
-    for sensor_id in sensor_ids:
-        #TO-DO return lat and long from openaq to use in wekeo call
-        fetch_sensor_data(sensor_id)
-        wekeo_api_call("2023-10-02T00:00:00.000000Z","2023-10-04T00:00:00.000000Z", lat, lon)
-else: 
-    print("Please enter a valid choice")
-    choice = input("Please enter your choice: ")
+while not validChoice:
+    if (choice ==1): 
+        print("Please enter the sensor id of the sensor you want to build a model for: ")
+        sensor_id = input()
+        validChoice = True
+    elif (choice ==2): 
+        print("You've chosen multiple sensors, building a model with 5 sensors in belgium...")
+        sensor_ids = [4878] #TO-DO: get 5 sensors in belgium
+        for sensor_id in sensor_ids:
+            lat, lon = fetch_sensor_data(sensor_id,"2023-10-02", "2023-10-04" )
+            wekeo_api_call("2023-10-02T00:00:00.000000Z","2023-10-04T00:00:00.000000Z", lat, lon)
+            validChoice = True
+    else: 
+        print("Please enter a valid choice")
+        choice = input("Please enter your choice: ")
 
 
 
