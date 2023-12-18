d0f60e639c54c89494e6f4a30a898872b88cf937
Sten Hulsbergen <s122441@ap.be>
2023-12-18 11:09:40 +0100

    Add longer timeout for geomapping requests

--- backend/FastAPI/main.py
+++ backend/FastAPI/main.py
@@ -85,7 +85,7 @@ async def locations():
             country_data = {}
             regions = []
             for region in records_regions:
-                locations = geo.geocode(region, country_codes=country_codes, language="en").raw["display_name"]
+                locations = geo.geocode(region, country_codes=country_codes, language="en", timeout=10).raw["display_name"]
                 print(locations)
                 locations = locations.split(", ")
                 if locations[-1] == country_name:
