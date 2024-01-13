807f19272ecc20488840cf5ab1da6302c455804f
Viral Ganga Persad <viral.ganga@gmail.com>
2024-01-13 04:36:33 +0100

    Improved homepage

--- frontend/src/app/(framed)/page.tsx
+++ frontend/src/app/(framed)/page.tsx
@@ -1,31 +1,15 @@
 "use client";
-import { useLocations } from "@/src/hooks/locations";
+import { useEffect, useState } from "react";
+import axios from "axios";
 import { Callout, Text, Title } from "@tremor/react";
 import { ExclamationCircleIcon } from "@heroicons/react/24/outline";
-
+import { AirQualityMeasurement } from "@/src/types/AirsightTypes";
 
 export default function Page() {
-  const { data: locations, isSuccess, isLoadingVisual, isError } = useLocations();
   return (
     <>
       <Title>Homepage of AIrsight</Title>
       <Text>Lorem ipsum dolor sit amet, consetetur sadipscing elitr.</Text>
-
-      {isLoadingVisual &&
-      <Callout
-      className="h-12 mt-4"
-      title="Loading locations"
-      icon={ExclamationCircleIcon}
-      color="rose"
-    >
-      Loading locations
-    </Callout>
-      }
-      {isSuccess &&
-      locations &&
-      locations.map(location => (
-        <p>{location}</p>
-      ))}
     </>
   );
 }
