b66a67ee087bd3314def588b31d2bb7bd1d1f06c
Sten H <s122441@ap.be>
2024-01-14 15:12:59 +0100

    Fix config

--- backend/FastAPI/main.py
+++ backend/FastAPI/main.py
@@ -185,5 +185,4 @@ async def specific_data_by_param_and_id(param: PARAMETERS_ENUM, id: str, data: s
 
 
 if __name__ == "__main__":
-    run("main:app", host="0.0.0.0", port=5000,
-        proxy_headers=True, forwarded_allow_ips=['*'], reload=True)
+    run("main:app", host="0.0.0.0", port=6000, proxy_headers=True, forwarded_allow_ips=['*'], workers=2)
